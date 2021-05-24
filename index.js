4f4c17d79f81cbb64f11f10446e8851eebf2d7b9
vojta <vmkproduction@outlook.cz>
2021-05-24 12:42:11 +0200

    Href to Each

---
+++ index.js
@@ -0,0 +1,8 @@
+function buttonPressed(){
+    if(document.getElementById('IDNadpis').style.color=="#ff0000"){
+        document.getElementById('IDNadpis').style.color="#00ff00";
+    }
+    else{
+        document.getElementById('IDNadpis').style.color="#ff0000";
+    }
+}
